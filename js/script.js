71441b6ae5981526f8bcc8fd4649811085228ce6
Tscharana <171447830+Tscharana@users.noreply.github.com>
2025-12-26 05:46:34 +0100

    Create data-value for every js-calc-input class and make handler take that

--- js/script.js
+++ js/script.js
@@ -7,6 +7,6 @@ const calculator = {
 const clicked = document.querySelector("#calculator");
 clicked.addEventListener("click", (e) => {
     if (e.target.classList.contains("js-calc-input")) { 
-    calculator.handleInput(e.target.textContent);
+    calculator.handleInput(e.target.dataset.value);
     }
 })
