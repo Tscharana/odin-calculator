2ffaf4d996490bc7d2b08e5b627b50a54a216e21
Tscharana <171447830+Tscharana@users.noreply.github.com>
2025-12-26 06:21:14 +0100

    Create handleInput() distribution to either processNumber() or processOperator()

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
 const calculator = {
+    firstValue: null,
+    firstOperator: null,
+    secondValue: null,
+    secondOperator: null,
+
+    allowedNumbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."],
+    allowedOperators: ["+", "-", "*", "/", "="],
+
     handleInput (input) {
-        console.log(input);
-    }
+        if (this.allowedNumbers.includes(input)) {
+            this.processNumber(input);
+        } else if (this.allowedOperators.includes(input)) {
+            this.processOperator(input);
+        }
+    },
+    processNumber (number) {
+    console.log("Number recieved: " + number);
+    },
+
+    processOperator (operator) {
+    console.log("Operator recieved: " + operator);
+    },
+
 }
 
 const clicked = document.querySelector("#calculator");
