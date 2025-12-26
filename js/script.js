bb53ae12b1ccabebd50b1713baf4a627e8d1a844
Tscharana <171447830+Tscharana@users.noreply.github.com>
2025-12-26 09:01:37 +0100

    Create core processOperator() for first operator and add functionality for processNumber() for secondValue and the .-case

--- js/script.js
+++ js/script.js
@@ -22,17 +22,36 @@ const calculator = {
     processNumber (number) {
         console.log("Number recieved: " + number + ".");
         if (this.waitingForSecondValue === false) {
-            if (this.firstValue === null) {
+            if (number === "." && this.firstValue && this.firstValue.includes(".")) {
+                return;
+            } else if (this.firstValue === null) {
                 this.firstValue = number;
             } else {
                 this.firstValue += number;
             }
+        } else {
+            if (number === "." && this.secondValue && this.secondValue.includes(".")) {
+                return;
+            } else if (this.secondValue === null) {
+                this.secondValue = number;
+            } else {
+                this.secondValue += number;
+            }
         }
-        console.log("FirstValue is " + this.firstValue);
+        console.log("firstValue is " + this.firstValue);
+        console.log("secondValue is " + this.secondValue);
     },
 
-        processOperator (operator) {
+    processOperator (operator) {
         console.log("Operator recieved: " + operator + ".");
+        if (this.firstValue !== null) {
+            if (this.secondValue === null) {
+                this.firstOperator = operator;
+            }
+            this.waitingForSecondValue = true;
+        }
+        console.log("firstOperator is " + this.firstOperator);
+        console.log("waitingForSecondValue is " + this.waitingForSecondValue);
     },
 
     processFunction (func) {
@@ -42,8 +61,9 @@ const calculator = {
 }
 
 const clicked = document.querySelector("#calculator");
+
 clicked.addEventListener("click", (e) => {
     if (e.target.classList.contains("js-calc-input")) { 
     calculator.handleInput(e.target.dataset.value);
     }
-})
+})  
