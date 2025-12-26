17bff23a2e771cefd5f995579d110e37c3eb4877
Tscharana <171447830+Tscharana@users.noreply.github.com>
2025-12-26 08:08:53 +0100

    Create core processNumber() logic for firstValue and create barebone processfunction()

--- js/script.js
+++ js/script.js
@@ -3,23 +3,40 @@ const calculator = {
     firstOperator: null,
     secondValue: null,
     secondOperator: null,
+    waitingForSecondValue: false,
 
     allowedNumbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."],
     allowedOperators: ["+", "-", "*", "/", "="],
+    allowedFunctions: ["CLEAR"],
 
     handleInput (input) {
         if (this.allowedNumbers.includes(input)) {
             this.processNumber(input);
         } else if (this.allowedOperators.includes(input)) {
             this.processOperator(input);
+        } else if (this.allowedFunctions.includes(input)) {
+            this.processFunction(input);
         }
     },
+
     processNumber (number) {
-    console.log("Number recieved: " + number);
+        console.log("Number recieved: " + number + ".");
+        if (this.waitingForSecondValue === false) {
+            if (this.firstValue === null) {
+                this.firstValue = number;
+            } else {
+                this.firstValue += number;
+            }
+        }
+        console.log("FirstValue is " + this.firstValue);
     },
 
-    processOperator (operator) {
-    console.log("Operator recieved: " + operator);
+        processOperator (operator) {
+        console.log("Operator recieved: " + operator + ".");
+    },
+
+    processFunction (func) {
+        console.log("Function key recieved: " + func + ".");
     },
 
 }
